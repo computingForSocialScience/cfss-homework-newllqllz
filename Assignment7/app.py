e481a14fb6305c126597a4a12069813453801ac6
linzhuo li <linzhuoli@uchicago.edu>
2015-03-12 12:25:17 -0500

    part 2

--- Assignment7/app.py
+++ Assignment7/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pymysql
+from fetchAlbums import *
+from fetchArtist import *
+from artistNetworks import *
+from analyzeNetworks import *
+import random
 
 dbname="playlists"
 host="localhost"
 user="root"
-passwd=""
+passwd="llz2.2360679"
 db=pymysql.connect(db=dbname, host=host, user=user,passwd=passwd, charset='utf8')
 
 app = Flask(__name__)
@@ -19,7 +24,14 @@ def make_index_resp():
 
 @app.route('/playlists/')
 def make_playlists_resp():
-    return render_template('playlists.html',playlists=playlists)
+    cur = db.cursor()
+    sql = "SELECT id, rootArtist FROM playlist;"
+    cur.execute(sql)
+    playlists_dic={}
+    for tup in cur.fetchall():
+        #print name
+        playlists_dic[tup[0]]=tup[1]
+    return render_template('playlists.html',playlists=playlists_dic)
 
 
 @app.route('/playlist/<playlistId>')
@@ -40,6 +52,68 @@ def add_playlist():
 
 
 
+def createNewPlaylist(artist_name):
+    cur = db.cursor()
+    sql_create_playlists ='''CREATE TABLE IF NOT EXISTS playlist (id INTEGER PRIMARY KEY AUTO_INCREMENT,rootArtist VARCHAR(128));'''
+    sql_create_songs ='''CREATE TABLE IF NOT EXISTS songs (playlistId INTEGER, songOrder INTEGER,artistName VARCHAR(128), albumName VARCHAR(256), trackName VARCHAR(256));'''
+    cur.execute(sql_create_playlists)
+    cur.execute(sql_create_songs)
+
+
+    sql_insert_artist_name = "INSERT INTO playlist (rootArtist) VALUES ('%s');" % (artist_name)
+    cur.execute(sql_insert_artist_name)
+
+    sql_select_id = """SELECT id FROM playlist;"""
+    cur.execute(sql_select_id)
+    playlistId = cur.fetchall()[0][0]
+    print playlistId
+    #print type(playlistId)
+    
+
+    #get artistlist:
+    depth=2
+    artist_id = fetchArtistId(artist_name)
+    edgelist = getEdgeList(artist_id,depth)
+    FinalArtistDic = {}
+    i = 0
+    while i<50:
+        point = randomCentralNode(pandasToNetworkX(edgelist))
+        if not FinalArtistDic.has_key(point):
+            FinalArtistDic[point]=1
+            i +=1
+        if i>2:
+            break
+        
+    FinalArtistList = FinalArtistDic.keys()
+
+    for artist in FinalArtistList:
+        tempList = fetchAlbumIds(artist)
+
+        ranNum = len(tempList)-1
+        randomAlbum = tempList[random.randint(0,ranNum)]
+        album_name = '"'+fetchAlbumInfo(randomAlbum)['name'].replace("'","")+'"'
+        artist_name2 ='"'+fetchArtistInfo(artist)['name'].replace("'","")+'"'
+        TrackUrl = "https://api.spotify.com/v1/albums/"+randomAlbum+"/tracks?limit=1"
+        track_name ='"'+requests.get(TrackUrl).json()['items'][0]['name'].replace("'","")+'"'
+        print artist_name2,'----', album_name,'----', track_name
+        #couldn't get it right
+        sql_insert_song_info = """INSERT INTO songs (playlistId,songOrder,artistName,albumName,trackName) VALUES ('%s','%s','%s','%s','%s');""" % (playlistId,FinalArtistList.index(artist)+1,artist_name2,album_name,track_name)
+        cur.execute(sql_insert_song_info)
+        
+        db.commit()
+
+    
+    sql_select_songs = """SELECT * FROM songs;"""
+    cur.execute(sql_select_songs)
+    songs_info = cur.fetchall()
+    for item in songs_info:
+        print item
+    cur.close()
+
+
+
+
 if __name__ == '__main__':
     app.debug=True
     app.run()
+    #createNewPlaylist("Beyonce")
